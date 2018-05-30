0b1eca4c1600788f9594dd66b31db712127d8c34
philtd5000 <philiptaft9@gmail.com>
2018-05-29 17:31:58 -0700

    tinkering and thinking

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+console.log("CONNECTED");
+
+$('document').ready(function(){
+
+        //Format: [0]=Name, [1]="Image", [2]="Hit Points", [4]="Attack Points"
+let yodaData    = ["Yoda", "../images/yoda-resized.jpg", 90, 6];
+let leiaData    = ["Princess Leia", "../images/princess-leia-resized.png", 100, 7];
+let anakinData  = ["Young Anakin", "../images/young-anakin-resized.jpg", 140, 9];
+let sheevData   = ["Sheev Palpatine", "../images/sheev-palpatine-resized.png", 130, 11];    
+
+
+
+let $characterCards = $('.img-container');
+
+let $enemiesRow = $('.enemies-row');
+
+//CONTINUE TESTING NEXT TIME:
+$enemiesRow.html("<div class="img-container"><div class="char-name">" + anakinData[0] + "</div>
+<img src="./assets/images/sheev-palpatine-resized.png" alt="sheev-palpatine">
+</div>"")
+
+
+
+
+})
