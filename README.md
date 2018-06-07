dad4e0c81d43934ff3b9f1c62f2832c48427383c
philtd5000 <philiptaft9@gmail.com>
2018-06-07 12:34:18 -0700

    condensed promptGameOver() + promptChamp() into one function and other changes

--- README.md
+++ README.md
@@ -2,18 +2,14 @@
 
 ## GAMEPLAY:
 
-* Select your character using mouse click
-* Select one of three enemy players to attack first
-* Battle the "Current Defender" by clicking "Attack" button
-* NOTE: Attack button will not display until there is an enemy prepared for it in "Current Defender" row
-* Choose your battles wisely with a "save the best for last" approach
+* SELECT your character using mouse click
+* SELECT one of three enemy players to attack first
+* BATTLE the "Current Defender" by clicking "Attack" button
+* *ATTENTION:* Attack button will not display until there is an enemy prepared for it in "Current Defender" row
+* CHOOSE your battles wisely with a "save the boss for last" approach...
 
 
 ### DEV NOTES
 
-#### BUGS / IMPROVEMENTS
-* clear game commentary text after win or loss 
-* no sound effects
-* WET code
-* can condense promptChampion() + promptGameOver() into one function
-* use two colors for game commentary (one for you attacked __ for __ HP) and other...
+#### IMPROVEMENTS:
+* TWO COLORS FOR GAME COMMENTARY
--- assets/script/game.js
+++ assets/script/game.js
@@ -2,7 +2,7 @@ $('document').ready(function () {
 
         //INSTANTIATION OF PLAYER CARDS FROM CONSTRUCTOR FUNCTION (BOTTOM OF SCRIPT):
         let yoda   = new CreateCharacter('Yoda', 'yoda', './assets/images/yoda-resized.jpg', 95, 14);
-        let leia   = new CreateCharacter('Princess Leia', 'leia', './assets/images/princess-leia-resized.png', 115, 100);
+        let leia   = new CreateCharacter('Princess Leia', 'leia', './assets/images/princess-leia-resized.png', 115, 27);
         let anakin = new CreateCharacter('Young Anakin', 'anakin', './assets/images/young-anakin-resized.jpg', 130, 17);
         let sheev  = new CreateCharacter('Sheev Palpatine', 'sheev', './assets/images/sheev-palpatine-resized.png', 120, 12);
 
@@ -12,20 +12,26 @@ $('document').ready(function () {
         let enemyChars   = [];
         let defenderChar = [];
 
-        let restartBtnInterval;
-        let restartBtnInterval2;
-
         //ROW TARGETS:
         let $topRow    = $('.top-row');
         let $middleRow = $('.middle-row');
         let $bottomRow = $('.bottom-row');
 
-        //MOVE PLAYER CARDS TO BOARD ON STARTUP:
+        //FLICKER INTERVAL:
+        let restartBtnInterval;
+
+        //WIN or LOSE:
+        let imgSrc      = "";
+        let win         = false;
+        let $audioWin   = $('audio#Win')[0];
+        let $audioLose  = $('audio#Lose')[0];
+
+        //RENDER GAME:
         cardsToBoard(allChars, $topRow);
         chooseYourCharacter();
         chooseDefender();
 
-        //BEGIN GAME FUNCTIONS:
+        //MAIN GAME FUNCTIONS:
         //=====================//
 
         function cardsToBoard(arr, target) {
@@ -59,7 +65,7 @@ $('document').ready(function () {
 
                         //APPEND CARDS TO BOARD AT TARGET:
                         target.append($playerCard);
-                } 
+                }
         }
 
         function chooseYourCharacter() {
@@ -209,7 +215,7 @@ $('document').ready(function () {
                         .css({ "font-size": "2em", "color": "#862800", "padding-top": "20px" });
 
                 //PLAYER AP INCREASED BY ORIGINAL baseAttack
-                playerChar[0].AP += playerBaseAttack; 
+                playerChar[0].AP += playerBaseAttack;
 
                 //REFLECT DAMAGE TO PLAYERCHAR:
                 $(`.hp-${playerClassName}`).text(playerChar[0].HP + "HP");
@@ -226,19 +232,22 @@ $('document').ready(function () {
                 let defenderHealth = defenderChar[0].HP;
 
                 if (defenderHealth <= 0 && enemyChars.length == 0 && playerHealth > 0) {
+                        win = true;
                         clearRow($bottomRow);
                         $('body').off('click', '.enemy');
                         $('body').off('click', '.attack');
                         $('.attack').toggleClass('invisible');
-                        promptChampion();
+                        promptWinOrLose(win);
                 } else if (defenderHealth <= 0 && enemyChars.length == 0 && playerHealth < 0) {
+                        win = true;
                         clearRow($topRow);
                         clearRow($bottomRow);
                         $('body').off('click', '.enemy');
                         $('body').off('click', '.attack');
                         $('.attack').toggleClass('invisible');
-                        promptChampion(); /*so if player dies.. but kills enemy.. still a legend? maybe add promptHero()*/
+                        promptWinOrLose(win); /*so if player dies.. but kills enemy.. still a legend? maybe add promptHero()*/
                 } else if (playerHealth <= 0) {
+                        win = false;
                         clearRow($topRow);
                         playerChar = [];
                         $('body').off('click', '.enemy');
@@ -248,7 +257,7 @@ $('document').ready(function () {
                         leia.reset();
                         anakin.reset();
                         sheev.reset();
-                        promptGameOver();
+                        promptWinOrLose(win);
                 } else if (defenderHealth <= 0) {
                         clearRow($bottomRow);
                         defenderChar = [];
@@ -256,131 +265,70 @@ $('document').ready(function () {
                 }
         }
 
-        function promptChampion() {
-                //HTML AND STYLING APPLIED ON THE FLY WITH JQUERY (practice rather than practicality):
-                let $modal    = $('<div id="modal">');
-                let $prompt   = $('<div id="prompt">');
-                let $gameOver = $('<div>');
-                let $restart  = $('<div>');
-                let $body = $('body');
-                
+        function promptWinOrLose(e) {
+
+                (e === true) ? $audioWin.play() : $audioLose.play();
+                (e === true) ? imgSrc = "./assets/images/you-win-oval.png" : imgSrc = "./assets/images/game-over-xs.png";
+                (e === true) ? promptTxt = "PLAY AGAIN" : promptTxt = "CONTINUE";
+
+                let $modal = $('<div id="modal">');
+                let $prompt = $('<div>');
+                let $restart = $('<div>');
+
                 $modal.css({ "position": "fixed", "top": "0", "left": "0", "bottom": "0", "right": "0", "background-color": "rgba(0,0,0,0.85)", "z-index": "5" });
-                // $prompt.css({ "background-color": "#0a0406", "border": "2px dashed slategray", "position": "fixed", "top": "50vh", "left": "50%", "transform": "translate(-50%, -50%)", "transition": "transform 1s", "z-index": "10" });
-                $gameOver.html('<img src=\"./assets/images/you-win-oval.png\" width="426px" height="220px">').css({"position": "fixed", "top": "80vh", "left": "50%", "transform": "translate(-50%, -200%)", "z-index":"100"})
-                
-                $restart.css({"position": "fixed", "top": "80%", "left": "50%", "font-size": "2rem", "cursor": "pointer", "z-index": "20", "transform": "translate(-50%, -50%)"});
-                $restart.html('<h2 id="restart">PLAY AGAIN?</h2>').css({ "opacity": "1", "color": "white", "z-index": "30", "transition-property": "opacity", "transition-duration": "1s"});
-                $restart.addClass('restart-hidden');
-                $body.append($restart);
-                clearInterval(restartBtnInterval);
+                $prompt.html(`'<img src="${imgSrc}" width="426px" height="220px">'`).css({ "position": "fixed", "top": "50vh", "left": "50vw", "transform": "translate(-50%, -50%)", "z-index": "100" })
+                $restart.css({ "position": "fixed", "top": "80%", "left": "50%", "font-size": "2rem", "cursor": "pointer", "z-index": "20", "transform": "translate(-50%, -50%)" });
+                $restart.html(`<h2>${promptTxt}</h2>`).addClass('restart');
+
+                $('body').append($restart);
+                $('body').append($modal);
+                $('body').append($prompt);
+
                 startFlicker();
+                restartButtonOnClick();
 
                 function startFlicker() {
-                        restartBtnInterval  = setInterval(appear, 1000);
+                        restartBtnInterval = setInterval(appear, 1000);
                 }
 
                 function appear() {
                         console.log("every 1s");
-                        $('#restart').toggleClass('restart-hidden');
-                }
-                //RESTART BUTTON CLICK LISTENER ADDED UPON CREATION:
-                $restart.on('click', function () {
-                        $prompt.hide();
-                        $modal.hide();
-                        $gameOver.hide();
-                        $restart.hide();
-                        clearInterval(restartBtnInterval);
-
-                        $('.commentary').empty();
-
-                        yoda.reset();
-                        leia.reset();
-                        anakin.reset();
-                        sheev.reset();
-
-                        clearRow($topRow);
-                        clearRow($middleRow);
-                        clearRow($bottomRow);
-
-                        playerChar = [];
-                        enemyChars = [];
-                        defenderChar = [];
-
-                        allChars = [yoda, leia, anakin, sheev];
-                        cardsToBoard(allChars, $topRow);
-                        chooseYourCharacter();
-                        chooseDefender();
-                })
-
-                $prompt.append($gameOver);
-
-                $('body').append($modal);
-                $('body').append($gameOver);
-        }
-
-        function promptGameOver() {
-                
-                //HTML AND STYLING APPLIED ON THE FLY WITH JQUERY (practice rather than practicality):
-                let $modal    = $('<div id="modal">');
-                let $prompt   = $('<div id="prompt">');
-                let $gameOver = $('<div>');
-                let $restart  = $('<div>');
-                let $body = $('body');
-                
-                $modal.css({ "position": "fixed", "top": "0", "left": "0", "bottom": "0", "right": "0", "background-color": "rgba(0,0,0,0.85)", "z-index": "5" });
-                // $prompt.css({ "background-color": "#0a0406", "border": "2px dashed slategray", "position": "fixed", "top": "50vh", "left": "50%", "transform": "translate(-50%, -50%)", "transition": "transform 1s", "z-index": "10" });
-                $gameOver.html('<img src=\"./assets/images/game-over-xs.png\" width="426px" height="220px">').css({"position": "fixed", "top": "80vh", "left": "50%", "transform": "translate(-50%, -200%)", "z-index":"100"})
-                
-                $restart.css({"position": "fixed", "top": "80%", "left": "50%", "font-size": "2rem", "cursor": "pointer", "z-index": "20", "transform": "translate(-50%, -50%)"});
-                $restart.html('<h2 id="restart">TRY AGAIN?</h2>').css({ "opacity": "1", "color": "white", "z-index": "30", "transition-property": "opacity", "transition-duration": "1s"});
-                $restart.addClass('restart-hidden');
-                $body.append($restart);
-                clearInterval(restartBtnInterval);
-                startFlicker();
-
-                function startFlicker() {
-                        restartBtnInterval  = setInterval(appear, 1000);
+                        $('.restart').toggleClass('restart-hidden');
                 }
 
-                function appear() {
-                        console.log("every 1s");
-                        $('#restart').toggleClass('restart-hidden');
+                function restartButtonOnClick() {
+                        //RESTART BUTTON CLICK LISTENER ADDED UPON CREATION:
+                        $restart.on('click', function () {
+                                $modal.hide();
+                                $prompt.hide();
+                                $restart.hide();
+                                clearInterval(restartBtnInterval);
+
+                                $('.commentary').empty();
+
+                                yoda.reset();
+                                leia.reset();
+                                anakin.reset();
+                                sheev.reset();
+
+                                clearRow($topRow);
+                                clearRow($middleRow);
+                                clearRow($bottomRow);
+
+                                playerChar = [];
+                                enemyChars = [];
+                                defenderChar = [];
+
+                                allChars = [yoda, leia, anakin, sheev];
+                                cardsToBoard(allChars, $topRow);
+                                chooseYourCharacter();
+                                chooseDefender();
+                        })
                 }
-                //RESTART BUTTON CLICK LISTENER ADDED UPON CREATION:
-                $restart.on('click', function () {
-                        $prompt.hide();
-                        $modal.hide();
-                        $gameOver.hide();
-                        $restart.hide();
-                        clearInterval(restartBtnInterval);
 
-                        $('.commentary').empty();
+        } /*end promptWinOrLose() */
 
-                        yoda.reset();
-                        leia.reset();
-                        anakin.reset();
-                        sheev.reset();
-
-                        clearRow($topRow);
-                        clearRow($middleRow);
-                        clearRow($bottomRow);
-
-                        playerChar = [];
-                        enemyChars = [];
-                        defenderChar = [];
-
-                        allChars = [yoda, leia, anakin, sheev];
-                        cardsToBoard(allChars, $topRow);
-                        chooseYourCharacter();
-                        chooseDefender();
-                })
-
-                $prompt.append($gameOver);
-
-                $('body').append($modal);
-                $('body').append($gameOver);
-        }
-        /* ^ can combine promptGameOver() + promptChampion() into endGamePrompt(e) */
+        //GAME TOOLS: 
 
         function updateArrays(player, enemyOne, enemyTwo, enemyThree, defender) {
                 playerChar = [player];
