097d2b6b47a3d956894cbe2dcb72fb10dc70ee60
philtd5000 <philiptaft9@gmail.com>
2018-06-08 14:37:41 -0700

    final review

--- README.md
+++ README.md
@@ -5,12 +5,7 @@
 * SELECT your character with mouse click
 * SELECT one of three enemy players to attack first
 * BATTLE the "Current Defender" by clicking "Attack" button
-* *ATTENTION:* Attack button will not display until there is an enemy prepared for it in "Current Defender" row
-* CHOOSE your battles wisely with a "save the boss for last" approach... (pay attention to enemy AP)
+* WIN by defeating all enemies.
+* **CHALLENGE:** Play as Yoda and WIN (without dying...)
 
 
-### DEV NOTES
-
-#### IMPROVEMENTS:
-* TWO COLORS FOR GAME COMMENTARY
-* BACKGROUND IMAGE
--- assets/script/game.js
+++ assets/script/game.js
@@ -1,10 +1,10 @@
 $('document').ready(function () {
 
         //INSTANTIATION OF PLAYER CARDS FROM CONSTRUCTOR FUNCTION (BOTTOM OF SCRIPT):
-        let yoda   = new CreateCharacter('Yoda', 'yoda', './assets/images/yoda-resized.jpg', 95, 14);
-        let leia   = new CreateCharacter('Princess Leia', 'leia', './assets/images/princess-leia-resized.png', 115, 27);
-        let anakin = new CreateCharacter('Young Anakin', 'anakin', './assets/images/young-anakin-resized.jpg', 130, 17);
-        let sheev  = new CreateCharacter('Sheev Palpatine', 'sheev', './assets/images/sheev-palpatine-resized.png', 120, 12);
+        let yoda   = new CreateCharacter('Yoda', 'yoda', './assets/images/yoda-resized.jpg', 95, 4);
+        let leia   = new CreateCharacter('Princess Leia', 'leia', './assets/images/princess-leia-resized.png', 105, 10);
+        let anakin = new CreateCharacter('Young Anakin', 'anakin', './assets/images/young-anakin-resized.jpg', 135, 5);
+        let sheev  = new CreateCharacter('Sheev Palpatine', 'sheev', './assets/images/sheev-palpatine-resized.png', 120, 7);
 
         //ARRAYS TO HOLD PLAYER OBJECTS
         let allChars     = [yoda, leia, anakin, sheev];
@@ -22,9 +22,9 @@ $('document').ready(function () {
 
         //WIN or LOSE:
         let imgSrc      = "";
-        let win         = false;
         let $audioWin   = $('audio#Win')[0];
         let $audioLose  = $('audio#Lose')[0];
+        let win;
 
         //RENDER GAME:
         cardsToBoard(allChars, $topRow);
@@ -39,10 +39,10 @@ $('document').ready(function () {
                 if (arr.length > 1) {
                         for (let i = 0; i < arr.length; i++) {
                                 //BUILDING PLAYER CARD HTML AND APPLYING APPROPRIATE CLASSES:
-                                let $playerCard = $('<div>').addClass('player-card ' + arr[i].className);
+                                let $playerCard = $('<div>').addClass(`player-card ${arr[i].className}`);
                                 let $playerName = $('<div>').addClass('char-name').html(arr[i].name);
                                 let $playerImage = $('<img>').attr('src', arr[i].imageURL);
-                                let $playerHP = $('<div>').addClass(`hp-${arr[i].className}`).html(arr[i].HP + 'HP');
+                                let $playerHP = $('<div>').addClass(`hp-${arr[i].className}`).html(`${arr[i].HP}HP`);
 
                                 //PLACE PLAYER ATTRIBUTES ON CARD:
                                 $playerCard.append($playerName);
@@ -53,10 +53,10 @@ $('document').ready(function () {
                                 target.append($playerCard);
                         }
                 } else if ((typeof arr) === 'object') {
-                        let $playerCard = $('<div>').addClass('player-card ' + arr[0].className);
+                        let $playerCard = $('<div>').addClass(`player-card ${arr[0].className}`);
                         let $playerName = $('<div>').addClass('char-name').html(arr[0].name);
                         let $playerImage = $('<img>').attr('src', arr[0].imageURL);
-                        let $playerHP = $('<div>').addClass(`hp-${arr[0].className}`).html(arr[0].HP + 'HP');
+                        let $playerHP = $('<div>').addClass(`hp-${arr[0].className}`).html(`${arr[0].HP}HP `);
 
                         //PLACE PLAYER ATTRIBUTES ON CARD:
                         $playerCard.append($playerName);
@@ -210,10 +210,14 @@ $('document').ready(function () {
                 defenderChar[0].HP -= playerChar[0].AP; /*player attacks*/
                 console.log(`You attacked ${defenderFullName} for ${playerChar[0].AP}hp damage!`);
 
-                $('.commentary') /* on game-screen USE TWO COLORS(!) */
-                        .html(`${defenderFullName} attacked you for ${defenderBaseAttack}HP damage! <br> You attacked ${defenderFullName} for ${playerChar[0].AP}HP damage!`)
-                        .css({ "font-size": "2em", "color": "#862800", "padding-top": "20px" });
+                $('.commentaryBoxOne')
+                        .html(`${defenderFullName} attacked you for ${defenderBaseAttack}HP damage!`)
+                        .css({ "font-size": "2em", "color": "#862800", "padding-top": "17.5px" });
 
+                $('.commentaryBoxTwo')
+                        .html(`You attacked ${defenderFullName} for ${playerChar[0].AP}HP damage!`)
+                        .css({ "font-size": "2em", "color": "green", "padding-top": "5px" });
+                        
                 //PLAYER AP INCREASED BY ORIGINAL baseAttack
                 playerChar[0].AP += playerBaseAttack;
 
@@ -230,6 +234,7 @@ $('document').ready(function () {
 
                 let playerHealth = playerChar[0].HP;
                 let defenderHealth = defenderChar[0].HP;
+                let defenderFullName = defenderChar[0].name; /*for game updates*/
 
                 if (defenderHealth <= 0 && enemyChars.length == 0 && playerHealth > 0) {
                         win = true;
@@ -237,7 +242,9 @@ $('document').ready(function () {
                         $('body').off('click', '.enemy');
                         $('body').off('click', '.attack');
                         $('.attack').toggleClass('invisible');
-                        promptWinOrLose(win);
+                        $('.commentaryBoxOne').empty();
+                        $('.commentaryBoxTwo').text(`You defeated ${defenderFullName}!`)
+                        promptWinOrLose(true);
                 } else if (defenderHealth <= 0 && enemyChars.length == 0 && playerHealth < 0) {
                         win = true;
                         clearRow($topRow);
@@ -245,7 +252,9 @@ $('document').ready(function () {
                         $('body').off('click', '.enemy');
                         $('body').off('click', '.attack');
                         $('.attack').toggleClass('invisible');
-                        promptWinOrLose(win); /*so if player dies.. but kills enemy.. still a legend? maybe add promptHero()*/
+                        $('.commentaryBoxOne').text(`You died but...`);
+                        $('.commentaryBoxTwo').text(`You defeated ${defenderFullName}!`)
+                        promptWinOrLose(true);
                 } else if (playerHealth <= 0) {
                         win = false;
                         clearRow($topRow);
@@ -257,18 +266,19 @@ $('document').ready(function () {
                         leia.reset();
                         anakin.reset();
                         sheev.reset();
-                        promptWinOrLose(win);
+                        promptWinOrLose(false);
                 } else if (defenderHealth <= 0) {
                         clearRow($bottomRow);
                         defenderChar = [];
-                        alert("SAVAGE!");
+                        $('.commentaryBoxOne').empty();
+                        $('.commentaryBoxTwo').text(`You defeated ${defenderFullName}!`)
                 }
         }
 
         function promptWinOrLose(e) {
 
                 (e === true) ? $audioWin.play() : $audioLose.play();
-                (e === true) ? imgSrc = "./assets/images/you-win-oval.png" : imgSrc = "./assets/images/game-over-xs.png";
+                (e === true) ? imgSrc = "./assets/images/you-win-oval.png" : imgSrc = "./assets/images/game-over-oval.png";
                 (e === true) ? promptTxt = "PLAY AGAIN" : promptTxt = "CONTINUE";
 
                 let $modal = $('<div id="modal">');
@@ -276,8 +286,8 @@ $('document').ready(function () {
                 let $restart = $('<div>');
 
                 $modal.css({ "position": "fixed", "top": "0", "left": "0", "bottom": "0", "right": "0", "background-color": "rgba(0,0,0,0.85)", "z-index": "5" });
-                $prompt.html(`'<img src="${imgSrc}" width="426px" height="220px">'`).css({ "position": "fixed", "top": "50vh", "left": "50vw", "transform": "translate(-50%, -50%)", "z-index": "100" })
-                $restart.css({ "position": "fixed", "top": "80%", "left": "50%", "font-size": "2rem", "cursor": "pointer", "z-index": "20", "transform": "translate(-50%, -50%)" });
+                $prompt.html(`'<img src="${imgSrc}" width="426px" height="220px">'`).css({ "position": "fixed", "top": "40vh", "left": "50vw", "transform": "translate(-50%, -50%)", "z-index": "100" })
+                $restart.css({ "position": "fixed", "top": "65%", "left": "50%", "font-size": "2rem", "cursor": "pointer", "z-index": "20", "transform": "translate(-50%, -50%)" });
                 $restart.html(`<h2>${promptTxt}</h2>`).addClass('restart');
 
                 $('body').append($restart);
@@ -304,7 +314,8 @@ $('document').ready(function () {
                                 $restart.hide();
                                 clearInterval(restartBtnInterval);
 
-                                $('.commentary').empty();
+                                $('.commentaryBoxOne').empty();
+                                $('.commentaryBoxTwo').empty();
 
                                 yoda.reset();
                                 leia.reset();
@@ -329,6 +340,21 @@ $('document').ready(function () {
         } /*end promptWinOrLose() */
 
         //GAME TOOLS: 
+
+        // function customPrompt(PromptText) {
+                
+        //         let str = PromptText.toString();
+
+        //         let $customPrompt = $('<div>')
+        //                 .html(`<h3>${str}</h3>`)
+        //                 .css({'background-color': 'white', 'border-radius': '5px', 'position': 'fixed', 'top': '20vh', 'left': '50vw', 'transform': 'translate(-50%, -50%)'});
+
+        //         $customPrompt.on('click', function() {
+        //                 $(this).hide();
+        //         })
+
+        //         $('body').append($customPrompt);
+        // }
 
         function updateArrays(player, enemyOne, enemyTwo, enemyThree, defender) {
                 playerChar = [player];
